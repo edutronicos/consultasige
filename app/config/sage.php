60b57637f5202b2358d994f64846e2543ad45024
edutronicos <amgp@email.com>
2025-02-21 20:24:37 -0300

    configuração do banco de dados: adicionado arquivo de configuração para SAGE com detalhes de conexão

---
+++ app/config/sage.php
@@ -0,0 +1,10 @@
+<?php
+return [
+    'host'  =>  "10.1.1.239\SQLEXPRESS",
+    'port'  =>  "56404",
+    'name'  =>  "SAGE",
+    'user'  =>  "dev",
+    'pass'  =>  "phrh@5020",
+    'type'  =>  "sql",
+    'prep'  =>  "1"
+];
